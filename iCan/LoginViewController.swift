bcfbc7ce2133d08e26b262bbc084f392f5c95cec
Kendrick Choate <kendrickchoate@gmail.com>
2016-12-04 13:18:47 -0600

    Updated LoginViewController to work with API once API call is available for login. Added custom tab bar view controller to hold the logged in session. Updated Main.storyboard to have password text appear as hidden and updated some constraints in view to align with set constraints.

--- iCan/LoginViewController.swift
+++ iCan/LoginViewController.swift
@@ -5,12 +5,24 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoggedInUser {
+    var UID: String? = nil
+}
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var usernameTextLabel: UITextField!
+    @IBOutlet weak var passwordTextLabel: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    let amazonKey: String = "https://0944tu0fdb.execute-api.us-west-2.amazonaws.com/prod/"
+    var loggedInUser: LoggedInUser? = nil
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let requestURL: URL = URL(string: "https://0944tu0fdb.execute-api.us-west-2.amazonaws.com/prod/jobs")!
+        loginButton.isEnabled = false
+        usernameTextLabel.delegate = self
+        passwordTextLabel.delegate = self
+        /*let requestURL: URL = URL(string: "https://0944tu0fdb.execute-api.us-west-2.amazonaws.com/prod/jobs")!
         let urlRequest: URLRequest = URLRequest(url: requestURL)
         let session = URLSession.shared
         let task = session.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
@@ -35,23 +47,82 @@ class LoginViewController: UIViewController {
             }
             
         })
-        task.resume()
-        
+        task.resume()*/
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
-    /*
+    @IBAction func LoginPressed() {
+        if (usernameTextLabel.text == nil || passwordTextLabel.text == nil || usernameTextLabel.text == "" || passwordTextLabel.text == "") {
+            presentErrorNotification(errorTitle: "Invalid Login", errorMessage: "Username or password left blank.")
+        }
+        else if (testLogin(userName: usernameTextLabel.text!, password: passwordTextLabel.text!)) {
+            performSegue(withIdentifier: "LoginSuccess", sender: self)
+        }
+        else {
+            presentErrorNotification(errorTitle: "Invalid Login", errorMessage: "Your username/password combination was not recognized.")
+        }
+    }
+    
+    func testLogin(userName: String, password: String) -> Bool{
+        //send username+password to server
+        //retrieve data
+        //check for response T or F
+        //if T: 
+            //save user info
+            //return true
+        //if F:
+            //return false
+        return false
+    }
+    
+    //a utility function for presenting an error notification to the user
+    func presentErrorNotification(errorTitle: String, errorMessage: String) {
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    // MARK: - Delegates
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // The next two if statements disable the login button until information is entered into the username and password fields
+        
+        if textField == usernameTextLabel
+        {
+            let oldStr = usernameTextLabel.text! as NSString
+            let newStr = oldStr.replacingCharacters(in: range, with: string) as NSString
+            if (newStr.length == 0 || passwordTextLabel.text!.characters.count == 0) {
+                loginButton.isEnabled = false
+            }
+            else {
+                loginButton.isEnabled = true
+            }
+        }
+        if textField == passwordTextLabel
+        {
+            let oldStr = passwordTextLabel.text! as NSString
+            let newStr = oldStr.replacingCharacters(in: range, with: string) as NSString
+            if (newStr.length == 0 || usernameTextLabel.text!.characters.count == 0) {
+                loginButton.isEnabled = false
+            }
+            else {
+                loginButton.isEnabled = true
+            }
+        }
+        return true
+    }
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "LoginSuccess" {
+            let destinationVC = segue.destination as! TabBarController
+            destinationVC.loggedInUser = self.loggedInUser
+        }
     }
-    */
 /* 
  The icons we use for the tab bar come from a website that asks us to link to each icon in our About page. The links are as such:
  search: https://icons8.com/web-app/5269/search-property
